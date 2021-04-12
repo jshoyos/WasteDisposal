766e927b40c5f1a39eb6af1feb2f1105d3fc0c1f
Juan Hoyos <juanse.jsh@gmail.com>
2021-04-12 14:19:11 -0400

    classification setup

--- app/src/main/java/com/example/waste_disposal_classification/ui/camera/CameraFragment.java
+++ app/src/main/java/com/example/waste_disposal_classification/ui/camera/CameraFragment.java
@@ -10,8 +10,10 @@ import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -22,7 +24,12 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.waste_disposal_classification.R;
+import com.example.waste_disposal_classification.classifier.ImageClassification;
+import com.example.waste_disposal_classification.classifier.Recognition;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import static androidx.core.content.PermissionChecker.checkSelfPermission;
@@ -32,9 +39,11 @@ public class CameraFragment extends Fragment {
     private static final int CAMERA_PERMISSION_REQUEST_CODE = 1000;
     private static final int CAMERA_REQUEST_CODE = 1001;
     private CameraViewModel cameraViewModel;
+    private ImageClassification imageClassifier;
     private Button captureBtn;
     private Button uploadBtn;
     private ImageView imageView;
+    private ListView listViewPrediction;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -42,12 +51,12 @@ public class CameraFragment extends Fragment {
                 new ViewModelProvider(this).get(CameraViewModel.class);
         View root = inflater.inflate(R.layout.fragment_camera, container, false);
         initUIElements(root);
-        cameraViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                //textView.setText(s);
-            }
-        });
+//        cameraViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                //textView.setText(s);
+//            }
+//        });
         return root;
     }
 
@@ -55,7 +64,12 @@ public class CameraFragment extends Fragment {
         captureBtn = root.findViewById(R.id.btn_capture_image);
         uploadBtn = root.findViewById(R.id.btn_upload_image);
         imageView = root.findViewById(R.id.camera_capture);
-
+        listViewPrediction = root.findViewById(R.id.listview_prediction);
+        try {
+            imageClassifier = new ImageClassification(getActivity());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         captureBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -109,6 +123,13 @@ public class CameraFragment extends Fragment {
             Bitmap photo = (Bitmap) Objects.requireNonNull((data).getExtras()).get("data");
             imageView.setImageBitmap(photo);
             //TODO: send photo to image classification
+            List<Recognition> predictions = imageClassifier.recognizeImage(photo, 0);
+            final List<String> predictionsList = new ArrayList<>();
+            for(Recognition recog : predictions){
+                predictionsList.add(recog.getName() + " :::::: " + recog.getConfidence());
+            }
+            ArrayAdapter<String> predictionsAdapter = new ArrayAdapter<>(getContext(), R.layout.support_simple_spinner_dropdown_item, predictionsList);
+            listViewPrediction.setAdapter(predictionsAdapter);
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
