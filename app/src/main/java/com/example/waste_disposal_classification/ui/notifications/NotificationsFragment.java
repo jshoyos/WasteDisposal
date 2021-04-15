f76ba7ff60e6703ea02251a08b78ed51bd043d4e
Juan Hoyos <juanse.jsh@gmail.com>
2021-04-15 13:09:23 -0400

    api get

--- app/src/main/java/com/example/waste_disposal_classification/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/waste_disposal_classification/ui/notifications/NotificationsFragment.java
@@ -1,5 +1,10 @@
 package com.example.waste_disposal_classification.ui.notifications;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,15 +13,22 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.waste_disposal_classification.News.Articles;
+import com.example.waste_disposal_classification.News.News;
 import com.example.waste_disposal_classification.R;
+
+import java.io.IOException;
 
 public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
+    private int PERMISSION_ALL = 1;
+    String[] PERMISSIONS = { Manifest.permission.INTERNET};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +42,28 @@ public class NotificationsFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        if (!hasPermission(getContext(), PERMISSIONS)){
+            ActivityCompat.requestPermissions(getActivity(), PERMISSIONS, PERMISSION_ALL);
+        }
+        try {
+            Articles articles = News.getNews();
+            System.out.println(articles.getArticles().get(0).getAuthor());
+        } catch (IOException e) {
+            e.printStackTrace();
+            textView.setText("An error has occurred");
+        }
         return root;
+    }
+
+    private boolean hasPermission(Context context, String... permissions) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context!=null && permissions != null){
+           for (String permission : permissions){
+               if(ActivityCompat.checkSelfPermission(getContext(), permission) != PackageManager.PERMISSION_GRANTED){
+                   return false;
+               }
+           }
+        }
+        return true;
     }
 }
