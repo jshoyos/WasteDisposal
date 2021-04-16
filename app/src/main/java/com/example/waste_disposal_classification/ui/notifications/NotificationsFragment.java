a290c709f62077bc33d39c498db682362f46a34e
noahfreger <50425626+noahfreger@users.noreply.github.com>
2021-04-16 18:37:37 -0400

    removed commented code

--- app/src/main/java/com/example/waste_disposal_classification/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/waste_disposal_classification/ui/notifications/NotificationsFragment.java
@@ -26,42 +26,9 @@ public class NotificationsFragment extends Fragment {
         notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-       // initUIElements(root);
+
 
         return root;
     }
 
-//    private void initUIElements(View root) {
-//        captureBtn = root.findViewById(R.id.btn_capture_image);
-//        uploadBtn = root.findViewById(R.id.btn_upload_image);
-//        imageView = root.findViewById(R.id.camera_capture);
-//        listViewPrediction = root.findViewById(R.id.listview_prediction);
-//        try {
-//            imageClassifier = new ImageClassification(getActivity());
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        captureBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (hasPermission()){
-//                    openCamera();
-//                }
-//                else{
-//                    requestPermission();
-//                }
-//            }
-//        });
-//        uploadBtn.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v) {
-//                if(hasGalleryPermission()){
-//                    openGallery();
-//                }
-//                else{
-//                    requestGalleryPermission();
-//                }
-//            }
-//        });
-//    }
 }
